911607b6e5481f434ea0761a418bdabd378987e5
Kyle Stough <kstough00@gmail.com>
2020-08-06 18:10:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Dog
   def name=(dog_name)
     @this_dogs_name = dog_name
@@ -16,3 +17,8 @@ fido = Dog.new
 fido.name = "Fido"
  
 puts fido.name
+=======
+class Dog 
+  
+end
+>>>>>>> c515eac6afe17b52f3467a27a0a90b8a601116d1
